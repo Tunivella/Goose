d76b14f4238b39f7ebf4fec8f549d4eebd800e91
Nina Potoshina <npotoshina@MacBook-Nina.local>
2024-07-14 18:45:16 +0200

    raylib?

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "raylib.h"
+#include "raymath.h"
+
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [models] example - drawing billboards");
+
+    while(true) {}
+    return 0;
+}
